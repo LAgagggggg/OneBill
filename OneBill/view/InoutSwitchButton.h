a03365a6fb85d706d2ad3c2f4f639364e2b6a2c1
LAgagggggg <15807814960@163.com>
2018-07-21 12:13:34 +0800

    little refactor

---
+++ OneBill/view/InoutSwitchButton.h
@@ -0,0 +1,17 @@
+//
+//  inoutSwitchButton.h
+//  OneBill
+//
+//  Created by LAgagggggg on 2018/7/19.
+//  Copyright © 2018 ookkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface InoutSwitchButton : UIView
+@property BOOL isOut;
+@end
+
+NS_ASSUME_NONNULL_END
