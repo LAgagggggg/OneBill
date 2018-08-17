3bef464e85e34f6ee0e36b0a97336b8df065ced9
LAgagggggg <15807814960@163.com>
2018-08-18 00:00:39 +0800

    transition animation

---
+++ OneBill/OBMainControllerTransition.h
@@ -0,0 +1,18 @@
+//
+//  OBMainControllerTransition.h
+//  OneBill
+//
+//  Created by LAgagggggg on 2018/8/17.
+//  Copyright © 2018 ookkee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OBMainControllerTransition : NSObject <UIViewControllerAnimatedTransitioning>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OneBill/OBMainControllerTransition.m
@@ -0,0 +1,13 @@
+//
+//  OBMainControllerTransition.m
+//  OneBill
+//
+//  Created by LAgagggggg on 2018/8/17.
+//  Copyright © 2018 ookkee. All rights reserved.
+//
+
+#import "OBMainControllerTransition.h"
+
+@implementation OBMainControllerTransition
+
+@end
